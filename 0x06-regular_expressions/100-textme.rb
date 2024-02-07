2e3042beec4434725e5c859058c62c22d779456f
Chimenem Okandu <87863371+chimenemking@users.noreply.github.com>
2024-02-07 17:21:30 +0100

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+SENDER = ARGV[0].scan(/from:\+*\w*/).join[5..-1]
+RECEIVER = ARGV[0].scan(/to:\+*\w*/).join[3..-1]
+FLAGS = ARGV[0].scan(/flags:(.*?)\]/).join
+
+msg = SENDER + "," + RECEIVER + "," + FLAGS
+puts msg
